c9002a7c293bd42f3e624061947c53980c8c28c4
kpokharel96 <kabindrapokharel91@gmail.com>
2021-08-25 00:40:21 -0500

    Logs with Debugger

--- Phase3/logs/nodehw.js
+++ Phase3/logs/nodehw.js
@@ -10,9 +10,11 @@ if(fs.existsSync(path)){
 let lastname = readline.question("Enter your last name: ");
 let gender = readline.question("Enter your gender: ")
 let emailId = readline.questionEMail("Enter your email id: ")
+debugger;
 let time_date=new Date().toLocaleString();
     let records1 = JSON.parse(fs.readFileSync("records.json").toString());
     records1.push({firstname,lastname,gender,emailId,time_date});
+    debugger;
     fs.writeFileSync("records.json",JSON.stringify(records1))
 }
 else{ 
@@ -20,6 +22,7 @@ else{
     let lastname = readline.question("Enter your last name: ");
     let gender = readline.question("Enter your gender: ")
     let emailId = readline.questionEMail("Enter your email id: ")
+    debugger;
     let time_date=new Date().toLocaleString();
     let records=[]
         records.push({firstname,lastname,gender,emailId,time_date});
@@ -27,6 +30,7 @@ else{
 }
 answer=readline.question("Do you want to add some more records: Y/N: ")
 answer.toLowerCase()
+debugger;
 }while(answer=="y")
 
 }
